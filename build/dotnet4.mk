b6f21a86f52e2e7a004d71ac7b8a52965000a4ac
Ery Lee <ery.lee@gmail.com>
2013-12-03 22:31:33 +0800

    add struts2 dotnet4

--- build/dotnet4.mk
+++ build/dotnet4.mk
@@ -1,10 +1,20 @@
-include build/config.mk
-
 PRODUCT_NAME = dotnet4
-MODULE_NAME = ${PRODUCT_NAME}
-PRODUCT_DIR = ${PREFIX}/${PRODUCT_NAME}
-STATIC_DIR = ${PRODUCT_DIR}/WebimPlugin/WebimPlugin/UI/static
-DIST_DIR = ${UI_DIST_DIR}/${PRODUCT_NAME}
 
-include build/include.mk
+PROJECT_NAME = git@github.com:webim/webim-plugin-dotnet4
+
+REL = WebimPlugin
+
+REL_STATIC = WebimPlugin/UI/static
+
+include config.mk
+
+SRC_FILES = ${PRODUCT_DIR}/*.md \
+	    ${PRODUCT_DIR}/WebimPlugin/* \
+
+all:
+	@@mkdir -p ${REL_DIR}
+	@@cp -r ${SRC_FILES} ${REL_DIR}/
+	@@cp -R ${STATIC_DIR}/* ${REL_STATIC_DIR}/
+	@@cd ${PRODUCT_DIST_DIR} && zip -r -q  ${REL_FILE} ${REL}
+	@@cd ${PRODUCT_DIST_DIR} && rm -rf ${REL}
 
--- build/struts2.mk
+++ build/struts2.mk
@@ -1,10 +1,29 @@
-include build/config.mk
-
 PRODUCT_NAME = struts2
-MODULE_NAME = ${PRODUCT_NAME}
-PRODUCT_DIR = ${PREFIX}/${PRODUCT_NAME}
-STATIC_DIR = ${PRODUCT_DIR}/Webim/src/static
-DIST_DIR = ${UI_DIST_DIR}/${PRODUCT_NAME}
 
-include build/include.mk
+PROJECT_NAME = git@github.com:webim/webim-plugin-struts2
+
+REL = Webim
+
+REL_STATIC = src/static
+
+include config.mk
+
+SRC_FILES = ${PRODUCT_DIR}/*.md \
+		${PRODUCT_DIR}/*.txt \
+	    ${PRODUCT_DIR}/Webim/.classpath \
+	    ${PRODUCT_DIR}/Webim/.project \
+	    ${PRODUCT_DIR}/Webim/.settings \
+	    ${PRODUCT_DIR}/Webim/WebContent \
+		${PRODUCT_DIR}/Webim/build \
+		${PRODUCT_DIR}/Webim/doc \
+		${PRODUCT_DIR}/Webim/resources \
+		${PRODUCT_DIR}/Webim/src \
+		${PRODUCT_DIR}/Webim/test \
+
+all:
+	@@mkdir -p ${REL_DIR}
+	@@cp -r ${SRC_FILES} ${REL_DIR}/
+	@@cp -R ${STATIC_DIR}/* ${REL_STATIC_DIR}/
+	@@cd ${PRODUCT_DIST_DIR} && zip -r -q  ${REL_FILE} ${REL}
+	@@cd ${PRODUCT_DIST_DIR} && rm -rf ${REL}
 
